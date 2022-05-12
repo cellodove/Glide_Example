65c0523ca5e0b05608ab302daf1f371afc9ae794
jusung.kim <wntjd200@naver.com>
2022-05-12 17:25:32 +0900

    이미지 출력

--- app/src/main/java/com/cellodove/glide_example/MainActivity.kt
+++ app/src/main/java/com/cellodove/glide_example/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.cellodove.glide_example
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
+import com.cellodove.glide_example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            Glide.with(this)
+                .load("https://picsum.photos/500/500")
+                .into(binding.image)
+        }
+
     }
 }
